6f7d3500470d2851c884a24022b719af8b20a402
José Willian <101588747+iwilliann@users.noreply.github.com>
2025-10-02 23:00:06 +0000

    Producer and Consumer

---
+++ eventos/src/main/java/br/univille/Producer.java
@@ -0,0 +1,27 @@
+package br.univille;
+
+import com.azure.core.amqp.AmqpTransportType;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+
+public class Producer {
+    public static void main(String[] args) {
+        var servidor = "sb-das1-2025.servicebus.windows.net";
+        var chave = System.getenv("CHAVE");
+        var queue = "queue-josewillian";
+
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+            .fullyQualifiedNamespace(servidor)
+            .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+            // .credential(credential)
+            .connectionString(chave)
+            .sender()
+            .queueName(queue)
+            .buildClient();
+
+        senderClient.sendMessage(new ServiceBusMessage("1: Corinthians roubado"));
+        senderClient.sendMessage(new ServiceBusMessage("2: Ninguem se importa"));
+        senderClient.sendMessage(new ServiceBusMessage("3: Ernomossauro da ZL"));
+    }
+}
